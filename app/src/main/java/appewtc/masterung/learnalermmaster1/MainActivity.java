372307ddf78a117e91c10111020997044b31134a
masterUNG <phrombutr@gmail.com>
2016-01-13 00:31:51 +0700

    กำหนด Time จาก TimePicker ได้แว้ววว

--- app/src/main/java/appewtc/masterung/learnalermmaster1/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnalermmaster1/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
@@ -35,10 +37,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showTimeTextView.setText("");
-
+                myTimePickerDialog(false);
             }   // event
         });
     }   // buttonController
+
+    private void myTimePickerDialog(boolean is24r) {
+
+        Calendar objCalendar = Calendar.getInstance();
+
+        objTimePickerDialog = new TimePickerDialog(MainActivity.this,
+                objOnTimeSetListener,
+                objCalendar.get(Calendar.HOUR_OF_DAY),
+                objCalendar.get(Calendar.MINUTE),
+                is24r);
+        objTimePickerDialog.setTitle("Please Set Time");
+        objTimePickerDialog.show();
+
+    }   // myTimePickerDialog
+
+    TimePickerDialog.OnTimeSetListener objOnTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+            Calendar curentCalendar = Calendar.getInstance();
+            Calendar setCalendar = (Calendar) curentCalendar.clone();
+
+            setCalendar.set(Calendar.HOUR_OF_DAY, i);
+            setCalendar.set(Calendar.MINUTE, i1);
+            setCalendar.set(Calendar.SECOND, 0);
+            setCalendar.set(Calendar.MILLISECOND, 0);
+
+            if (setCalendar.compareTo(curentCalendar) <= 0) {
+                setCalendar.add(Calendar.DATE, 1);
+            }
+            setAlarm(setCalendar);
+
+        }   // onTimeSet
+    };
+
+    private void setAlarm(Calendar setCalendar) {
+
+        //Show Alarm
+        showTimeTextView.setText("Time = " + setCalendar.getTime());
+
+    }   // setAlarm
 
     private void bindWidget() {
         clickButton = (Button) findViewById(R.id.button);
