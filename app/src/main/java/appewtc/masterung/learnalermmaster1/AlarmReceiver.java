66487e4c02436a48b6751e999ce25a8a8ecb6182
masterUNG <phrombutr@gmail.com>
2016-01-13 21:20:31 +0700

    Project success

--- app/src/main/java/appewtc/masterung/learnalermmaster1/AlarmReceiver.java
+++ app/src/main/java/appewtc/masterung/learnalermmaster1/AlarmReceiver.java
@@ -3,6 +3,7 @@ package appewtc.masterung.learnalermmaster1;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.widget.Toast;
 
 /**
@@ -12,5 +13,7 @@ public class AlarmReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Alarm Success", Toast.LENGTH_SHORT).show();
+        MediaPlayer objMediaPlayer = MediaPlayer.create(context, R.raw.phonton1);
+        objMediaPlayer.start();
     }   // onReceive
 }   // Main Class
